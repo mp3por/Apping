d1d186742014d1cd574c4df19514e917d44f6649
svchosta <raymond.svchost@gmail.com>
2014-01-24 04:23:05 +0000

    SplashScreenActivity + MainActivity

---
+++ src/uk/ac/gla/apping/quartet/businesscardapp/MainActivity.java
@@ -0,0 +1,21 @@
+package uk.ac.gla.apping.quartet.businesscardapp;
+
+import uk.ac.gla.apping.quartet.businnesscardapp.R;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+public class MainActivity extends Activity {
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		
+		setContentView(R.layout.activity_main);	
+	}
+}
