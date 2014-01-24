4f1e8989c14bfd964223c55de01d6380a3a9d5de
svchosta <raymond.svchost@gmail.com>
2014-01-24 04:44:19 +0000

    ImporterActivity + CardViewerActivity

---
+++ src/uk/ac/gla/apping/quartet/businesscardapp/ImporterActivity.java
@@ -0,0 +1,17 @@
+package uk.ac.gla.apping.quartet.businesscardapp;
+
+import uk.ac.gla.apping.quartet.businnesscardapp.R;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ImporterActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		
+		setContentView(R.layout.activity_importer);
+	}
+
+}
--- src/uk/ac/gla/apping/quartet/businesscardapp/MainActivity.java
+++ src/uk/ac/gla/apping/quartet/businesscardapp/MainActivity.java
@@ -11,11 +11,38 @@ import android.widget.Button;
 
 public class MainActivity extends Activity {
 	
+	Button mButtonAdd;
+	Button mButtonCard;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		
 		setContentView(R.layout.activity_main);	
+		
+		mButtonAdd = (Button) findViewById(R.id.buttonAdd);
+		mButtonAdd.setOnClickListener(new OnClickListener(){
+				
+			@Override
+			public void onClick(View arg0) {			
+				Intent intent = new Intent(MainActivity.this, ImporterActivity.class);
+				startActivity(intent);
+			}
+				
+		});
+		
+		
+		mButtonCard = (Button) findViewById(R.id.buttonCard);
+		mButtonCard.setOnClickListener(new OnClickListener(){
+			
+			@Override
+			public void onClick(View arg0) {			
+				Intent intent = new Intent(MainActivity.this, CardViewerActivity.class);
+				startActivity(intent);
+			}
+				
+		});
+		
 	}
 }
