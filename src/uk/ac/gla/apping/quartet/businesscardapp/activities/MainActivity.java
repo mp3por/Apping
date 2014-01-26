3b897b92bd0bead3d266343eaf9ed0871456d3da
svchosta <raymond.svchost@gmail.com>
2014-01-26 20:35:13 +0000

    menu for main activity

--- src/uk/ac/gla/apping/quartet/businesscardapp/activities/MainActivity.java
+++ src/uk/ac/gla/apping/quartet/businesscardapp/activities/MainActivity.java
@@ -4,9 +4,13 @@ import uk.ac.gla.apping.quartet.businnesscardapp.R;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 	
@@ -41,4 +45,29 @@ public class MainActivity extends Activity {
 		});
 		
 	}
+
+	
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+    	MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.main, menu);
+        return true;
+    }
+    
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+	        case R.id.menuItemAdvancedSearch:
+	        	// do something with the advanced search... maybe, maybe not.
+	        	CharSequence text = "Pending...";
+	        	Toast toast = Toast.makeText(this, text, Toast.LENGTH_SHORT);
+	        	toast.show();
+	        	
+	            return true;
+	        default:
+	            return super.onOptionsItemSelected(item);
+	    }
+	}
+	
+	
 }
